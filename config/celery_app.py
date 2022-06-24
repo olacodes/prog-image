4137133fb92f5017f7f0d845f01b62aaeaa3165a
Olatunde Sodiq <olatundesodiq@gmail.com>
2022-06-24 11:48:05 +0100

    add default value to celery

--- config/celery_app.py
+++ config/celery_app.py
@@ -5,8 +5,8 @@ from celery import Celery
 env = environ.Env()
 
 app = Celery('app')
-app.conf.broker_url = env("CELERY_BROKER_URL")
-app.conf.result_backend = env("CELERY_RESULT_BACKEND")
+app.conf.broker_url = env("CELERY_BROKER_URL", default='redis://redis:6379/0')
+app.conf.result_backend = env("CELERY_RESULT_BACKEND", default="redis://redis:6379/0")
 app.conf.imports = ['storage_service.app.tasks']
 # app.conf.task_serializer = 'pickle'
 # app.conf.result_serializer = 'pickle'
