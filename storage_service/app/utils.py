2f4732b236436a31c76adb3e05dda6a8aa7096f6
Olatunde Sodiq <olatundesodiq@gmail.com>
2022-07-04 18:51:24 +0100

    Refactor storage service

--- storage_service/app/utils.py
+++ storage_service/app/utils.py
@@ -1,9 +1,51 @@
 import random
 import string
 import uuid
+from io import BytesIO
+
+import requests
+from PIL import Image
+from requests.exceptions import RequestException
+
+from storage_service.global_variables import SUPPORTED_FORMATS
+from storage_service.logger import Logger
+
+logger = Logger(__name__)
+log = logger.log()
 
 
-def generate_id():
-    random_letter = random.choice(string.ascii_letters)
-    uuid_hex = str(uuid.uuid4().hex)
-    return f'{random_letter}{uuid_hex}'.lower()
+class Utils:
+    @staticmethod
+    def generate_id():
+        random_letter = random.choice(string.ascii_letters)
+        uuid_hex = str(uuid.uuid4().hex)
+        return f'{random_letter}{uuid_hex}'.lower()
+
+    @staticmethod
+    async def save_file(file, location, is_file=True):
+        with Image.open(file) if is_file else Image.open(BytesIO(file)) as fd:
+            fd.save(location)
+            return location
+
+    @staticmethod
+    async def request_url(url):
+        try:
+            response = requests.get(url)
+            return response.content
+        except RequestException as e:
+            log.error(e)
+
+    @staticmethod
+    def convert(filepath):
+        converted_files = []
+        for format in SUPPORTED_FORMATS:
+            try:
+                image = Image.open(filepath)
+                image = image.convert('RGB')
+                new_filepath = filepath.split(".")[0]
+                new_filepath = f'{new_filepath}.{format}'
+                image.save(new_filepath)
+                converted_files.append(new_filepath)
+            except OSError as e:
+                log.error(e)
+        return converted_files
--- storage_service/s3/s3_service.py
+++ storage_service/s3/s3_service.py
@@ -1,6 +1,7 @@
 import boto3
 import botocore
 import environ
+from botocore.exceptions import ClientError
 
 from storage_service.logger import Logger
 
@@ -9,8 +10,9 @@ AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', default=None)
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', default=None)
 AWS_REGION = env('AWS_REGION', default=None)
 AWS_S3_BUCKET = env('AWS_S3_BUCKET', default=None)
-AWS_S3_STORAGE_KEY = env('AWS_S3_STORAGE_KEY', default='storage_service/')
-
+AWS_S3_STORAGE_KEY = env('AWS_S3_STORAGE_KEY', default='storage_service')
+AWS_S3_BASE_URL = env(
+    'AWS_S3_BASE_URL', default='https://sodiqcodesbucket.s3.amazonaws.com')
 logger = Logger(__name__)
 
 
@@ -18,17 +20,12 @@ class S3Service:
     S3 = boto3.resource('s3')
     log = logger.log()
 
-    def __init__(self, aws_access_key_id, aws_secret_access_key, region, *args, **kwargs):
-        self.aws_access_key_id = aws_access_key_id
-        self.aws_secret_access_key = aws_secret_access_key
-        self.region = region
-
     @classmethod
     async def get_file(cls, filename):
         try:
             KEY = f"{AWS_S3_STORAGE_KEY}/{filename}"
             cls.S3.Bucket(env('AWS_S3_BUCKET')).download_file(KEY, filename)
-            return f'https://{env("AWS_S3_BUCKET")}.s3.amazonaws.com/{KEY}'
+            return {'url': f'https://{env("AWS_S3_BUCKET")}.s3.amazonaws.com/{KEY}'}
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
                 cls.log.error(e)
@@ -41,8 +38,9 @@ class S3Service:
         try:
             res = cls.S3.Bucket(f"{AWS_S3_BUCKET}")
             print(prefix)
-            return [f'https://{file.bucket_name}.s3.amazonaws.com/{file.key}'
+            urls = [f'https://{file.bucket_name}.s3.amazonaws.com/{file.key}'
                     for file in res.objects.filter(Prefix=f"{AWS_S3_STORAGE_KEY}/{prefix}")]
+            return {'urls': urls}
 
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
@@ -50,16 +48,30 @@ class S3Service:
                 return {"error": "The object does not exist."}
             else:
                 cls.log.error(e)
+                return {"error": str(e)}
 
     @classmethod
     async def get_all(cls):
         try:
             res = cls.S3.Bucket(f"{AWS_S3_BUCKET}")
-            return [f'https://{file.bucket_name}.s3.amazonaws.com/{file.key}'
+            urls = [f'https://{file.bucket_name}.s3.amazonaws.com/{file.key}'
                     for file in res.objects.all()]
+            return {'urls': urls}
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
                 cls.log.error(e)
-                return "The object does not exist."
+                return{"error": "The object does not exist."}
             else:
                 cls.log.error(e)
+                return{"error": str(e)}
+
+    @classmethod
+    def upload_file_s3(cls, filepath):
+        try:
+            filename = filepath.split('/')[-1]
+            key = f'{AWS_S3_STORAGE_KEY}/{filename}'
+            cls.S3.meta.client.upload_file(
+                filepath, AWS_S3_BUCKET, Key=key)
+            return f"{AWS_S3_BASE_URL}/{key}"
+        except ClientError as e:
+            cls.log.error(e)
