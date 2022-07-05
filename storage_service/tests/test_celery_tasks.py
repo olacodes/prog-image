cd1a35380eb57c77998ffb84713c9c379e2543fc
Olatunde Sodiq <olatundesodiq@gmail.com>
2022-07-05 09:52:18 +0100

    add links and depends on

--- storage_service/tests/test_celery_tasks.py
+++ storage_service/tests/test_celery_tasks.py
@@ -5,16 +5,14 @@ import unittest
 import environ
 from celery import Celery
 
-from config.celery_app import app
 from storage_service.global_variables import SUPPORTED_FORMATS
 
 env = environ.Env()
 
-# BROKER_URL = env('REDIS_URL', default="redis://redis:6379")
-# REDIS = env('REDIS_URL', default="redis://redis:6379")
+BROKER_URL = env('REDIS_URL', default="redis://redis:6379/0")
+REDIS = env('REDIS_URL', default="redis://redis:6379/0")
 
-# app = Celery('test', broker=BROKER_URL, backend=REDIS)
-
+app = Celery('test', broker=BROKER_URL, backend=REDIS)
 
 
 TMP_TEST_FILES = env(
